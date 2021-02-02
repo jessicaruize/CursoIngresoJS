ea79b73c8e260cf1698e3d9eef5844b900ec635f
jessicaruize <jessicaruizz@gmail.com>
2021-02-01 23:28:28 -0300

    commit

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,13 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	//reservo espacio en memoria para guardar el nombre del ususario
+	let nombre;
+
+	//guardo el la variable nombre el texto que escribio el usuario dentro de la ventana prompt
+nombre = prompt ("ingrese su nombre");
+
+	//copio el nombre que tengo guardado en la variable nombre dentro de la caja de texto enbebida en la pagina
+document.getElementById(txtIdNombre).value = nombre;
 }
 
