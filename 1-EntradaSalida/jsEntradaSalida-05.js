913afe7a17f316d81158326fafac9895009afc78
jessicaruize <jessicaruizz@gmail.com>
2021-02-02 09:32:38 -0300

    commit5

--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -4,10 +4,15 @@ ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
 	//creamos variables para guardar los datos del usuario y conseguimos el valor de cada caja de texto con el Id.
-	let nombre = document.getElementById('txtIdNombre').value;
-	let edad = document.getElementById('txtIdEdad').value;
 	
-	alert ("Usted se llama " + nombre+ " y tiene " + edad + " años." );
+let nombre
+let apellido
+let edad
+nombre = document.getElementById('txtIdNombre').value;
+apellido = document.getElementById('txtIdNombre').value;
+edad = document.getElementById('txtIdEdad').value;
+	
+alert ("Usted se llama " + nombre + apellido + " y tiene " + edad + " años." );
 	
 	
 }
