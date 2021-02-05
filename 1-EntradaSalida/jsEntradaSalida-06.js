65b1014ec704dd2c3e4864f5c9a019a68acf1e96
jessicaruize <jessicaruizz@gmail.com>
2021-02-04 21:48:37 -0300

    commit

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,17 +4,20 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	let numeroUno;
-	let numeroDos;
+	let num1;
+	let num2;
 	let sumar;
-numeroUno = document.getElementById('txtIdNumeroUno');
-numeroDos = document.getElementById('txtIdNumeroDos');
+    num1 = document.getElementById('txtIdNumeroUno');
+	num2 = document.getElementById('txtIdNumeroDos');
 
-numeroUno = parseInt(numeroUno);
-numeroDos = parseInt(numeroDos);
 
-sumar = numeroUno + numeroDos;
-alert("la suma es:" + sumar);
+	alert (num1);
+	alert (num2);
+	//num1 = parseInt(num1);
+	//num2 = parseInt(num2);
+
+	//sumar = num1 + num2;
+	//alert("la suma es:" + sumar);
 
 //parseInt() paranumeros entero
 //parsefloat() para numeros decimales
